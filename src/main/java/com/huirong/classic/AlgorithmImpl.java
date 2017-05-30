389756ad84a123d17c8144a814f28242e4e953d8
nanhuirong <nanhuirong@gmail.com>
2017-05-30 11:06:47 +0800

    2017-05-31

--- src/main/java/com/huirong/classic/AlgorithmImpl.java
+++ src/main/java/com/huirong/classic/AlgorithmImpl.java
@@ -1,5 +1,7 @@
 package com.huirong.classic;
 
+import java.util.Arrays;
+
 /**
  * Created by huirong on 17-5-22.
  */
@@ -105,5 +107,128 @@ public class AlgorithmImpl {
         }
         return result;
     }
+
+    /**
+     * 利用字符串解决大数问题（利用数组解决大数问题）
+     * 打印出1到最大的n位数
+     */
+    public void printToMaxOfDigits(int n){
+        if (n <= 0){
+            return;
+        }
+        char[] array = new char[n];
+        for (int i = 0; i < array.length; i++){
+            array[i] = '0';
+        }
+        while (!increment(array)){
+            printNumber(array);
+        }
+    }
+
+    /**
+     * 模拟大数加法
+     */
+    private boolean increment(char[] array){
+        boolean isOverflow = false;
+        int nTakeOver = 0;
+        for (int i = array.length - 1; i >= 0; i--){
+            int nSum = array[i] - '0' + nTakeOver;
+            if (i == array.length - 1){
+                nSum++;
+            }
+            if (nSum >= 10){
+                //如果进位发生在第0位，则返回true
+                if (i == 0){
+                    isOverflow = true;
+                }else {
+                    nSum -= 10;
+                    nTakeOver = 1;
+                    array[i] = (char) ('0' + nSum);
+                }
+            }else {
+                array[i] = (char)('0' + nSum);
+                break;
+            }
+        }
+        return isOverflow;
+    }
+    private void printNumber(char[] array){
+        boolean isBeginning0 = true;
+        for (int i = 0; i < array.length; i++){
+            if (isBeginning0 && array[i] != '0'){
+                isBeginning0 = false;
+            }
+            if (!isBeginning0){
+                System.out.print(array[i]);
+            }
+        }
+        System.out.print("\t");
+    }
+
+    public static void main(String[] args) {
+        AlgorithmImpl test = new AlgorithmImpl();
+//        test.printToMaxOfDigits(2);
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        test.reorderOddEven(array);
+        System.out.println(Arrays.toString(array));
+    }
+
+    /**
+     * 调整数组顺序，使得奇数位于偶数前面
+     */
+    public void reorderOddEven(int[] array){
+        if (array == null || array.length <= 0){
+            return;
+        }
+        int head = 0, tail = array.length - 1;
+        while (head < tail){
+            //向后移动数据，直到遇上偶数
+            while (head < tail && (array[head] & 0x1) != 0){
+                head++;
+            }
+            //向前移动，直到遇上奇数
+            while (head < tail && (array[tail] & 0x1) == 0){
+                tail--;
+            }
+            if (head < tail){
+                swap(array, head, tail);
+            }
+        }
+    }
+
+
+
+    private void swap(int[] array, int index1, int index2){
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
+
+    /**
+     * 荷兰国旗问题
+     *  三色0， 1， 2
+     *  三个指针head，tail，current
+     *      遇到1，current指针移动，不变动位置
+     *      遇到0，current与head交换位置,begin++ current++
+     *      遇到2，与end交换位置
+     */
+    public void control(int[] array)throws Exception{
+        int head = 0, curreent = 0, tail = array.length - 1;
+        while (curreent <= tail){
+            if (array[curreent] == 0){
+                swap(array, curreent, head);
+                head++;
+                curreent++;
+            }else if (array[curreent] == 1){
+                curreent++;
+            }else if (array[curreent] == 2){
+                swap(array, curreent, tail);
+                tail--;
+            }else {
+                throw new Exception("");
+            }
+        }
+    }
+
 
 }
--- src/main/java/com/huirong/list/MyLinkedList.java
+++ src/main/java/com/huirong/list/MyLinkedList.java
@@ -1,16 +1,13 @@
 package com.huirong.list;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Stack;
 
 /**
  * Created by huirong on 17-5-22.
  */
 public class MyLinkedList {
-    private Node head;
 
-    public void addTail(int value){
+    public Node addTail(Node head, int value){
         Node node = new Node(value);
         if (head == null){
             head = node;
@@ -21,10 +18,14 @@ public class MyLinkedList {
             }
             pointer.next = node;
         }
+        return head;
     }
 
-    //删除第一个含有某值的节点
-    public void remove(int value){
+    /**
+     *  删除第一个含有某值
+     * @param value
+     */
+    public void remove(Node head,int value){
         if (head == null){
             return;
         }
@@ -52,7 +53,7 @@ public class MyLinkedList {
      * 1.利用栈实现该功能，后进先出，鲁棒性更好
      * 2.利用递归实现，问题是当链表很长是容易出现栈溢出
      */
-    public void printList(){
+    public void printReserveList1(Node head){
         //递归
 //        printList(head);
         //利用栈实现
@@ -67,14 +68,119 @@ public class MyLinkedList {
             System.out.print(String.format("%d\t", pointer.value));
         }
     }
-    private void printList(Node node){
-        if (node != null){
-            if (node.next != null){
-                printList(node.next);
+    private void printReserveList2(Node head){
+        if (head != null){
+            if (head.next != null){
+                printReserveList2(head.next);
             }
-            System.out.println(String.format("%d\t", node.value));
+            System.out.print(String.format("%d\t", head.value));
         }
     }
+
+    public void printList(Node head){
+        Node pointer = head;
+        while (pointer != null){
+            System.out.print(pointer.value + " ");
+            pointer = pointer.next;
+        }
+    }
+
+    /**
+     * 给定单向链表的头指针和一个节点指针，在O（1）时间内删除该节点
+     * 该方法必须考虑删除节点在链表中
+     */
+    public void deleteNode(Node head,Node node){
+        if (head == null || node == null){
+            return;
+        }
+        //要删除的节点不是尾部节点
+        if (node.next != null){
+            node.value = node.next.value;
+            node.next = node.next.next;
+        }else if (head == node){
+            //删除节点是头节点
+            head = null;
+        }else {
+            //删除节点是尾部节点
+            Node pointer = head;
+            while (pointer.next != node){
+                pointer = pointer.next;
+            }
+            pointer.next = null;
+        }
+    }
+
+    /**
+     * 链表的倒数第k个节点
+     *  引伸题目：
+     *  求链表的中间节点
+     *      定义两个指针，同时从头开始，第一个指针一次走一步，第二个指针一次走两步
+     *  判断单链表是否存在环
+     *      定义两个指针，从头开始，第一个指针一次走一步，第二个指针一次走两步，如果相遇则存在环
+     *
+     */
+    public Node findKthToTail(Node head,int k){
+        if (head == null || k <= 0){
+            return null;
+        }
+        Node pointer1 = head, pointer2 = null;
+        for (int i = 0; i < k - 1; i++){
+            if (pointer1.next != null){
+                pointer1 = pointer1.next;
+            }else {
+                return null;
+            }
+        }
+        pointer2 = head;
+        while (pointer1.next != null){
+            pointer1 = pointer1.next;
+            pointer2 = pointer2.next;
+        }
+        return pointer2;
+    }
+
+    /**
+     * 反转链表
+     */
+    public void reserveList(Node head){
+        if (head == null){
+            return;
+        }
+        Node newHead = null, pointer = head, temp = null;
+        while (pointer != null){
+            temp = pointer.next;
+            if (newHead == null){
+                newHead = pointer;
+                newHead.next = null;
+            }else {
+                pointer.next = newHead;
+                newHead = pointer;
+            }
+            pointer = temp;
+        }
+        head = newHead;
+    }
+
+    /**
+     * 升序链表合并
+     */
+    public Node merge(Node node1, Node node2){
+        if (node1 == null){
+            return node2;
+        }else if (node2 == null){
+            return node1;
+        }
+        Node mergeHead = null;
+        if (node1.value < node2.value){
+            mergeHead = node1;
+            mergeHead.next = merge(node1.next, node2);
+        }else {
+            mergeHead = node2;
+            mergeHead.next = merge(node1, node2.next);
+        }
+        return mergeHead;
+    }
+
 
     //单向链表
     private class Node{
@@ -89,13 +195,28 @@ public class MyLinkedList {
 
     public static void main(String[] args){
         MyLinkedList list = new MyLinkedList();
+        Node head = null;
         for (int i = 0; i < 10; i++){
-            list.addTail(i);
+            list.addTail(head, i);
         }
-        list.remove(0);
-        list.remove(9);
-        list.remove(5);
-        list.printList();
+//        list.printList(head);
+//        list.reserveList(head);
+//        System.out.println();
+//        list.printList(head);
+        Node head1 = null;
+        for (int i = 0; i < 10; i = i + 2){
+            head1 = list.addTail(head1, i);
+        }
+        list.printList(head1);
+        System.out.println();
+        Node head2 = null;
+        for (int i = 1; i < 10; i = i + 2){
+            head2 = list.addTail(head2, i);
+        }
+        list.printList(head2);
+        Node head3 = list.merge(head1, head2);
+        System.out.println();
+        list.printList(head3);
 
     }
 }
